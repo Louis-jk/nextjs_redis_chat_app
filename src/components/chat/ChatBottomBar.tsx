9a726d25fdb728de6ceee1496e03fc9123e8cf11
Joonho Kim <lippoint.surf0622@gmail.com>
2025-03-31 18:24:33 +0900

    feat: Add keyboard sound effect to the text area

--- src/components/chat/ChatBottomBar.tsx
+++ src/components/chat/ChatBottomBar.tsx
@@ -6,13 +6,29 @@ import { Textarea } from "../ui/textarea";
 import { useState, useRef } from "react";
 import EmojiPicker from "./EmojiPicker";
 import { Button } from '../ui/button';
+import useSound from 'use-sound';
+import { usePreferences } from '@/store/usePreferences';
 
 const ChatBottomBar = () => {
     const [message, setMessage] = useState('');
     const textAreaRef = useRef<HTMLTextAreaElement>(null);    
     const isPending = false;
+    const { soundEnabled } = usePreferences();
+    const [playSound1] = useSound("/sounds/keystroke1.mp3");
+    const [playSound2] = useSound("/sounds/keystroke2.mp3");
+    const [playSound3] = useSound("/sounds/keystroke3.mp3");
+    const [playSound4] = useSound("/sounds/keystroke4.mp3");
 
-	return <div className="p-2 flex justify-between w-full items-center gap-2">
+    const playSoundFunctions = [playSound1, playSound2, playSound3, playSound4];
+
+    const playRandomKeyStrokeSound = () => {
+        if (soundEnabled) {
+            const randomIndex = Math.floor(Math.random() * playSoundFunctions.length);
+            playSoundFunctions[randomIndex]();
+        }
+    }
+
+    return <div className="p-2 flex justify-between w-full items-center gap-2">
         {!message.trim() && <ImageIcon size={20} className="cursor-pointer text-muted-foreground" />}
         <AnimatePresence>
             <motion.div 
@@ -37,7 +53,10 @@ const ChatBottomBar = () => {
                     rows={1}
                     className="w-full border rounded-full flex items-center h-9 resize-none overflow-hidden bg-background min-h-0"
                     value={message}
-                    onChange={(e) => setMessage(e.target.value)}
+                    onChange={(e) => {
+                        setMessage(e.target.value);
+                        playRandomKeyStrokeSound();
+                    }}
                 />
                 <div className="absolute right-2 bottom-0.5">
                     <EmojiPicker
